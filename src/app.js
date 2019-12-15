7a9f63147279f6c6b781d528b6a27d26280f84ce
anesh612 <aneshkaul@gmail.com>
2019-12-15 11:55:06 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 const app = express()
+const port = process.env.PORT || 3000
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
@@ -63,4 +64,4 @@ app.get('*', (req, res) => {
         name: 'Anesh Kaul'
     })
 })
-app.listen(3000)
+app.listen(port)
